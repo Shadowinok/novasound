b0a1fce0fcb652e215df1c1df6eed0dc540cfa77
Shadowinok <shadowinok@gmail.com>
2026-03-22 19:56:23 +1000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import YandexMetrika from './components/YandexMetrika';
 import { AuthProvider } from './context/AuthContext';
 import { PlayerProvider } from './context/PlayerContext';
 import './index.css';
@@ -9,6 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <YandexMetrika />
       <AuthProvider>
         <PlayerProvider>
           <App />
