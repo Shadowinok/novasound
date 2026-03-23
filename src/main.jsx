0444d30e1bfda39d78ea92a7c3099f19a85b4afa
Shadowinok <shadowinok@gmail.com>
2026-03-24 00:24:31 +1000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import YandexMetrika from './components/YandexMetrika';
+import GlobalToast from './components/GlobalToast';
 import { AuthProvider } from './context/AuthContext';
 import { PlayerProvider } from './context/PlayerContext';
+import { ToastProvider } from './context/ToastContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,9 +14,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <YandexMetrika />
       <AuthProvider>
-        <PlayerProvider>
-          <App />
-        </PlayerProvider>
+        <ToastProvider>
+          <PlayerProvider>
+            <App />
+            <GlobalToast />
+          </PlayerProvider>
+        </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
